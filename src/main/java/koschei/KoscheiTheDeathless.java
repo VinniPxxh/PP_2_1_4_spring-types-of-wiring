7a7516630e4ad062f8df02a21dd3e433dad895e0
VinniPxxh <kuznetsove2001@mail.ru>
2022-05-14 18:09:36 +0300

    Final commit

--- src/main/java/koschei/KoscheiTheDeathless.java
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -1,12 +1,12 @@
 package koschei;
 
+import koschei.models.Deth8;
 import koschei.models.Ocean1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KoscheiTheDeathless {
-
     private Ocean1 ocean;
 
     public String getRulesByDeth() {
